30524528d11d9e0fa0becbb63fcba512fe0d5270
Kirill Sokolov <zzzzz0908@yandex.ua>
2020-03-08 14:04:40 +0300

    Добавлен класс Extensions

---
+++ IndustrialGeodesy.Math/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndustrialGeodesy.Math
+{
+    public static class Extensions
+    {
+        public static Point3D Centroid(this List<Point3D> points)
+        {
+            int n = points.Count;
+
+            double northing = 0;
+            double easting = 0;
+            double height = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                northing += points[i].N;
+                easting += points[i].E;
+                height += points[i].H;
+            }
+
+            northing = northing / n;
+            easting = easting / n;
+            height = height / n;
+
+            return new Point3D(northing, easting, height);
+        }
+    }
+}
