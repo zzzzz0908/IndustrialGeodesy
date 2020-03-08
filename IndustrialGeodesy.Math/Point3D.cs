3ffb96497b06aa98e330a439466c1a43f45880f4
Kirill Sokolov <zzzzz0908@yandex.ua>
2020-03-08 14:03:03 +0300

    Добавлена структура Point3D

---
+++ IndustrialGeodesy.Math/Point3D.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace IndustrialGeodesy.Math
+{
+	public struct Point3D
+	{
+		public double N { get; private set; }
+		public double E { get; private set; }
+		public double H { get; private set; }
+
+		public Point3D(double northing, double easting, double height)
+		{
+			N = northing;
+			E = easting;
+			H = height;
+		}
+
+
+		public static Point3D operator +(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.N + p2.N, p1.E + p2.E, p1.H + p2.H);
+		}
+
+		public static Point3D operator -(Point3D p1, Point3D p2)
+		{
+			return new Point3D(p1.N - p2.N, p1.E - p2.E, p1.H - p2.H);
+		}
+
+
+		public double[] ToArray()
+		{
+			return new double[] { this.N, this.E, this.H };
+		}
+	}
+}
