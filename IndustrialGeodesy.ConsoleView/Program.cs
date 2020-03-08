f701b97cd50eba37a36928067a755c502ef9befa
Kirill Sokolov <zzzzz0908@yandex.ua>
2020-03-08 14:09:41 +0300

    Добавлен тест для аппроксимации плоскости

--- IndustrialGeodesy.ConsoleView/Program.cs
+++ IndustrialGeodesy.ConsoleView/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using IndustrialGeodesy.Math;
+using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 namespace IndustrialGeodesy.ConsoleView
 {
@@ -6,7 +11,34 @@ namespace IndustrialGeodesy.ConsoleView
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Point3D> points = new List<Point3D>(25);
+
+            string path = @"E:\Учеба\НИРС магистратура\Плоскость\test.txt";
+
+            string[] input = File.ReadAllLines(path);
+
+            foreach (string i in input)
+            {
+                double[] coords = Array.ConvertAll(i.Split(' '), temp => Convert.ToDouble(temp, CultureInfo.InvariantCulture));
+                points.Add(new Point3D(coords[0], coords[1], coords[2]));
+            }
+
+            var center = points.Centroid();
+
+            //points.ForEach(i => i -= center);
+
+            int n = points.Count;
+            Matrix<double> X = Matrix<double>.Build.Dense(n, 3);
+
+            for (int i = 0; i < n; i++)
+            {
+                points[i] -= center;
+                X.SetRow(i, points[i].ToArray());
+            }
+
+            var svd = X.Svd();
+
+            Console.WriteLine(svd.VT.Row(2));
         }
     }
 }
--- IndustrialGeodesy.Math/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace IndustrialGeodesy.Math
-{
-    public class Class1
-    {
-    }
-}
